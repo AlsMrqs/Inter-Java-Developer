ef1c5914d21fd30cdd23b0d8f1446c35aaa39e6c
AlsMrqs <alsmrqs@outlook.com>
2021-02-26 05:30:04 -0300

    Conheça finally e throw

---
+++ java/excecoes/InvalidRangeException.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class InvalidRangeException extends Exception {
+
+    public
+    InvalidRangeException () {
+        super("Invalid range!");
+    }
+
+    public
+    InvalidRangeException (String str) {
+        super(str);
+    }
+
+    public static void
+    main (String[] args) {
+        Number unity;
+
+        try {
+            unity = new Number(10);
+            System.out.println("Valid unity "+ unity.getNumber());
+        } catch (InvalidRangeException err) {
+            err.printStackTrace();
+        }
+    }
+}
+
+class Number {
+
+    private int number;
+
+    public
+    Number (int number) throws InvalidRangeException {
+        setNumber(number);
+    }
+
+    public void
+    setNumber (int number) throws InvalidRangeException {
+        if (number < -9 || number > 9) {
+            throw new InvalidRangeException("Invalid range!");
+        }
+        this.number = number;
+    }
+
+    public int
+    getNumber () { return number; }
+}
